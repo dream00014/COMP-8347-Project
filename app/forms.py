2c20fafb0e92fa2ee9cbe354036203dd3b75274b
Kiran Prasad Puthan <41895796+kiran867@users.noreply.github.com>
2023-11-20 21:30:38 -0500

    In , a custom file validator ensures only , , or  files are uploaded for the mandatory  field in , which also includes unique username and email validation and password confirmation checks.

--- app/forms.py
+++ app/forms.py
@@ -1,37 +1,19 @@
+from django.core.exceptions import ValidationError
 from django import forms
 from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
+import os
 
+def validate_file_extension(value):
+    ext = os.path.splitext(value.name)[1]  # Extracts the file extension
+    valid_extensions = ['.jpg', '.png', '.pdf']
+    if not ext.lower() in valid_extensions:
+        raise ValidationError('Unsupported file extension. Allowed types are .jpg, .png, .pdf')
 
 class RankingFilterForm(forms.Form):
-    RANKING_CHOICES = [
-        ("all", "All Rankings"),
-        ("top", "Top Rankings"),
-        ("worst", "Worst Rankings"),
-    ]
-
-    ranking_filter = forms.ChoiceField(
-        choices=RANKING_CHOICES,
-        label="Filter by Ranking",
-        widget=forms.Select(attrs={"class": "form-control"}),
-        initial="all",
-    )
-
+    # ... existing RankingFilterForm code ...
 
 class LoginForm(forms.Form):
-    username = forms.CharField(
-        max_length=100,
-        widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "Username"}
-        ),
-    )
-    password = forms.CharField(
-        max_length=100,
-        widget=forms.PasswordInput(
-            attrs={"class": "form-control", "placeholder": "Password"}
-        ),
-    )
-
+    # ... existing LoginForm code ...
 
 class SignUpForm(forms.Form):
     username = forms.CharField(
@@ -56,6 +38,11 @@ class SignUpForm(forms.Form):
         widget=forms.PasswordInput(
             attrs={"class": "form-control", "placeholder": "Confirm Password"}
         ),
+    )
+    id_document = forms.FileField(
+        required=True,
+        validators=[validate_file_extension],
+        widget=forms.FileInput(attrs={'class': 'form-control'})
     )
 
     def clean_username(self):
